fb52f9095c4e67f821de949120234e78ef4b0f0d
tophat8855 <hrfarley@gmail.com>
2015-04-05 19:38:56 -0700

    formatting and calculation of miles and emissions totals

--- app/controllers/leg.js
+++ app/controllers/leg.js
@@ -1,11 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  walkSegments: Ember.computed.filterBy('model', 'modeIsWalk'),
+  walkMiles: Ember.computed.mapBy('walkSegments', 'distance'),
+  walkEms: Ember.computed.mapBy('walkSegments', 'emissions'),
+  totalWalkEms: Ember.computed.sum('walkEms'),
+  totalWalk: Ember.computed.sum('walkMiles'),
+
   bikeSegments: Ember.computed.filterBy('model', 'modeIsBike'),
-  bikeMiles: Ember.computed.mapBy('bikeSegments', 'distance'), //for total mapBy('model', 'distance')
+  bikeMiles: Ember.computed.mapBy('bikeSegments', 'distance'),
+  bikeEms: Ember.computed.mapBy('bikeSegments', 'emissions'),
+  totalBikeEms: Ember.computed.sum('bikeEms'),
   totalBike: Ember.computed.sum('bikeMiles'),
 
-  //totalBike: Ember.computed.sum('bikeSegments.distance'),
+  busSegments: Ember.computed.filterBy('model', 'modeIsBus'),
+  busMiles: Ember.computed.mapBy('busSegments', 'distance'), //for total mapBy('model', 'distance')
+  busEms: Ember.computed.mapBy('busSegments', 'emissions'),
+  totalBusEms: Ember.computed.sum('busEms'),
+  totalBus: Ember.computed.sum('busMiles'),
+
+  bartSegments: Ember.computed.filterBy('model', 'modeIsBART'),
+  bartMiles: Ember.computed.mapBy('bartSegments', 'distance'),
+  bartEms: Ember.computed.mapBy('bartSegments', 'emissions'),
+  totalBartEms: Ember.computed.sum('bartEms'),
+  totalBART: Ember.computed.sum('bartMiles'),
+
+  allEms: Ember.computed.mapBy('model', 'emissions'),
+  totalEmissions: Ember.computed.sum('allEms'),
+  allMiles: Ember.computed.mapBy('model', 'distance'),
+  totalMiles: Ember.computed.sum('allMiles'),
 
   actions: {
     deleteLeg: function(leg) {
