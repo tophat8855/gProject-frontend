7442e89819cfb47bc4682ebcc210ba0736d2ab52
tophat8855 <hrfarley@gmail.com>
2015-04-07 14:40:22 -0700

    deploy to heroku attempt

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,7 +4,7 @@ import ENV from '../config/environment';
 export default DS.ActiveModelAdapter.extend({
   //host: 'http://localhost:3000'
   //slight hack until the ember-cli-dotenv package is fixed
-  host: ENV.adapterURL || ENV.ADAPTER_URL
+  //host: ENV.adapterURL || ENV.ADAPTER_URL
 //  host: 'http://carfreeapi.mybluemix.net'
-//  host: 'https://carfreeapi.herokuapp.com'
+  host: 'https://carfreeapi.herokuapp.com'
 });
